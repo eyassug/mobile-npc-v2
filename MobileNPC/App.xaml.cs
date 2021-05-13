166d08cc8b10ed728a2d9e55d3d0beb0958837bc
Eyassu Getachew <eyassug@gmail.com>
2021-05-14 00:38:57 +0300

    added AppCenter Analytics and Crashes

--- MobileNPC/App.xaml.cs
+++ MobileNPC/App.xaml.cs
@@ -3,6 +3,9 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using MobileNPC.Services;
 using MobileNPC.Views;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace MobileNPC
 {
@@ -19,6 +22,8 @@ namespace MobileNPC
 
         protected override void OnStart()
         {
+            AppCenter.Start("android=2978adb1-3a4b-4c3f-89fc-22cdd61d0c9c;ios=4675c0bf-7208-4873-969a-235251ceb5b0;",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
