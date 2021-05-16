7dbf2a23bad294f019842203b0f98213a73a61ae
Eyassu Getachew <eyassug@gmail.com>
2021-05-16 19:23:05 +0300

    initialized ZXing.NET in Android and iOS projects

--- MobileNPC.iOS/AppDelegate.cs
+++ MobileNPC.iOS/AppDelegate.cs
@@ -26,6 +26,7 @@ namespace MobileNPC.iOS
             Xamarin.Calabash.Start();
 #endif
             global::Xamarin.Forms.Forms.Init();
+            global::ZXing.Net.Mobile.Forms.iOS.Platform.Init();
             global::Xamarin.Forms.FormsMaterial.Init();
             LoadApplication(new App());
 
