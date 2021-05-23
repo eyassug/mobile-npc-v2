ee54733314ce96c4ded94e00f9ca96152af65696
Eyassu Getachew <eyassug@gmail.com>
2021-05-23 19:17:25 +0300

    added AppShell View & ViewModel

---
+++ MobileNPC/ViewModels/AppShellViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MobileNPC.ViewModels
+{
+    using ReactiveUI;
+    public class AppShellViewModel : ViewModelBase
+    {
+        public AppShellViewModel(IScreen hostScreen = null): base(hostScreen)
+        {
+        }
+    }
+}
---
+++ MobileNPC/Views/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using ReactiveUI;
+using ReactiveUI.XamForms;
+using MobileNPC.ViewModels;
+
+namespace MobileNPC.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppShell : ReactiveTabbedPage<AppShellViewModel>
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
