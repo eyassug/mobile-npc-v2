55b3b8e20fabb228fd7fa08018b623762b6021aa
Eyassu Getachew <eyassug@gmail.com>
2021-05-27 19:08:25 +0300

    added Sextant ExceptionHandler and BaseViewModel

---
+++ MobileNPC/ViewModels/BaseViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace MobileNPC.ViewModels
+{
+    using Sextant;
+    using ReactiveUI;
+    using Splat;
+    using System.ComponentModel;
+
+    public class BaseViewModel : ReactiveObject, INotifyPropertyChanged, IActivatableViewModel, IViewModel
+    {
+        public virtual string Id => "BaseViewModel";
+
+        public ViewModelActivator Activator { get; } = new ViewModelActivator();
+
+        protected readonly IViewStackService ViewStackService;
+
+        public BaseViewModel(IViewStackService viewStackService)
+        {
+            ViewStackService = viewStackService ?? Locator.Current.GetService<IViewStackService>();
+        }
+    }
+}
