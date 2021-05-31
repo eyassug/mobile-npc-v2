c57a0f83f400029d1c43fe79a9729e4be7a904e9
Eyassu Getachew <eyassug@gmail.com>
2021-05-31 20:58:56 +0300

    implemented ProductService and GS1Parser Service

---
+++ MobileNPC.Core/Services/GS1ParserService.cs
@@ -0,0 +1,33 @@
+﻿namespace MobileNPC.Core.Services
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class GS1ParserService : IGS1ParserService
+    {
+        const int MinLength = 13;
+        const int MaxLength = 14;
+        const string GtinIdentifier = "01";
+        public GS1ParserService()
+        {
+        }
+
+        public Dictionary<string, string> GetApplicationIdentifiers(string barcodeContent)
+        {
+            if (string.IsNullOrEmpty(barcodeContent)) throw new ArgumentException(nameof(barcodeContent));
+            throw new NotImplementedException();
+        }
+
+        public string GetGTIN(string barcodeContent)
+        {
+            if (string.IsNullOrEmpty(barcodeContent)) throw new ArgumentException(nameof(barcodeContent));
+            if (barcodeContent.Length == MinLength || barcodeContent.Length == MaxLength)
+                return barcodeContent;
+            var identifiers = GS1Net.Parse(barcodeContent);
+            if(identifiers.Any(i => i.Key.AI == GtinIdentifier))
+                return identifiers.Single(i => i.Key.AI == GtinIdentifier).Value;
+            throw new ArgumentException(nameof(barcodeContent));
+        }
+    }
+}
--- MobileNPC.Core/Services/IProductService.cs
+++ MobileNPC.Core/Services/IProductService.cs
@@ -5,12 +5,27 @@
 
     public interface IProductService
     {
+        ///// <summary>
+        ///// Gets a product with the specified identifier
+        ///// </summary>
+        ///// <param name="code">GTIN or Product Identification Code</param>
+        ///// <returns></returns>
+        //Task<Models.Product> GetProductAsync(string code);
+        ///// <summary>
+        ///// Gets a paginated list of products based on the specified filters
+        ///// </summary>
+        ///// <param name="queryString">initial querystring</param>
+        ///// <param name="page">Page number</param>
+        ///// <param name="limit">Number of items per page</param>
+        ///// <param name="locale">Akeneo Locale</param>
+        ///// <returns></returns>
+        //Task<IEnumerable<Models.Product>> GetProductsAsync(string queryString, int page = 1, int limit = 25, string locale="en_US");
         /// <summary>
         /// Gets a product with the specified identifier
         /// </summary>
         /// <param name="code">GTIN or Product Identification Code</param>
         /// <returns></returns>
-        Task<Models.Product> GetProductAsync(string code);
+        Task<Akeneo.Model.Product> GetProductAsync(string code);
         /// <summary>
         /// Gets a paginated list of products based on the specified filters
         /// </summary>
@@ -19,6 +34,15 @@
         /// <param name="limit">Number of items per page</param>
         /// <param name="locale">Akeneo Locale</param>
         /// <returns></returns>
-        Task<IEnumerable<Models.Product>> GetProductsAsync(string queryString, int page = 1, int limit = 25, string locale="en_US");
+        Task<IEnumerable<Akeneo.Model.Product>> GetProductsAsync(string queryString, int page = 1, int limit = 25, string locale = "en_US");
+        /// <summary>
+        /// Gets a paginated list of products based on the specified filters
+        /// </summary>
+        /// <param name="queryString">initial querystring</param>
+        /// <param name="page">Page number</param>
+        /// <param name="limit">Number of items per page</param>
+        /// <param name="locale">Akeneo Locale</param>
+        /// <returns></returns>
+        Task<IEnumerable<Akeneo.Model.Product>> GetProductsAsync(IEnumerable<Akeneo.Search.Criteria> criteria, int page = 1, int limit = 25, string locale = "en_US");
     }
 }
