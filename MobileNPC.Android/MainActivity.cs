ad4581465306c82c76527bd8a8c73a5f74d30b81
Eyassu Getachew <eyassug@gmail.com>
2021-08-09 16:04:51 +0300

    added Splash Screen with Malawi Coat of Arms

--- MobileNPC.Android/MainActivity.cs
+++ MobileNPC.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace MobileNPC.Droid
 {
-    [Activity(Label = "MobileNPC", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "MobileNPC", Icon = "@mipmap/icon", Theme = "@style/MainTheme", ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ MobileNPC.Android/SplashActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Android.App;
+using Android.OS;
+using Android.Util;
+using System.Threading.Tasks;
+using Android.Content;
+
+namespace MobileNPC.Droid
+{
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        static readonly string TAG = "X:" + typeof(SplashActivity).Name;
+
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+            Log.Debug(TAG, "SplashActivity.OnCreate");
+        }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { Startup(); });
+            startupWork.Start();
+        }
+
+        // TODO: Remove
+        // Simulates background work that happens behind the splash screen
+        async void Startup()
+        {
+            await Task.CompletedTask;
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
