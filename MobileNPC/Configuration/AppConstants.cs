a02016b045a5461955a29ae4bf7cd43a1c0df9ce
Eyassu Getachew <eyassug@gmail.com>
2022-04-28 14:25:04 +0300

    restored text and changed config to rwanda

--- MobileNPC/Configuration/AppConstants.cs
+++ MobileNPC/Configuration/AppConstants.cs
@@ -2,13 +2,13 @@
 {
     public class AppConstants
     {
-        public const string AkeneoUrl = "https://malawi.productcatalog.io";
-        public const string Username = "MobileApp";
-        public const string Password = "spinach-motet-endanger-wackiness-lew-ensue-yelling-undercut";
-        public const string ClientId = "4_5rre1nht3f4sswc08044gg8cs48ckk4ww8wsks00ws0k80sc8g";
-        public const string ClientSecret = "3w86lhrslv28socso00ccws00ws48go8sskcg0ogkgo4okggk4";
+        public const string AkeneoUrl = "";
+        public const string Username = "";
+        public const string Password = "";
+        public const string ClientId = "";
+        public const string ClientSecret = "";
         public const string AkeneoConfigUrl = "https://jsonblob.com/api/jsonBlob/1cc9a252-3dc5-11eb-ac66-6d9c480b2f3d";
-        public const string EnvironmentName = "Malawi";
+        public const string EnvironmentName = "Rwanda";
         public const string SplashThemeName = "@style/MyTheme.Splash"+EnvironmentName;
     }
 
